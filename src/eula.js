7a5e2e4b5c6b854d630c0a25169f2a1933ebe0cd
Noah Schutte <noahschutte@gmail.com>
2017-03-26 22:00:44 -0600

    add router

---
+++ src/eula.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const EULA = () => {
+  return (
+    <div>
+      <h1>End User License Agreement ("in knead")</h1>
+      <div id='ppBody'>
+        <div className='innerText'>Please read this End­ User License Agreement carefully before clicking the "I Agree" button, downloading or using the application ("in knead").<br/><br/></div>
+        <div className='innerText'>By clicking the "I Agree" button, downloading or using the Application, you are agreeing to be bound by the terms and conditions of this Agreement.<br/><br/></div>
+        <div className='innerText'>If you do not agree to the terms of this Agreement, do not click on the "I Agree" button and do not download or use the Application.<br/></div>
+        <span id='infoCo'></span><br/>
+        <div className='grayText'><strong>License</strong></div>
+        <div className='innerText'>"Buddy Ghee LLC", grants you a revocable, non­exclusive, non­transferable, limited license to download, install and use the Application solely for your personal, non­commercial purposes strictly in accordance with the terms of this Agreement.</div><br/>
+        <div className='grayText'><strong>Restrictions</strong></div>
+        <div className='innerText'>You agree not to, and you will not permit others to: a) license, sell, rent, lease, assign, distribute, transmit, host, outsource, disclose or otherwise commercially exploit the Application or make the Application available to any third party.</div>
+        <span id='infoUs'></span><br/>
+        <div className='grayText'><strong>Modifications to the Application</strong></div>
+        <div className='innerText'>"Buddy Ghee LLC", reserves the right to modify, suspend or discontinue, temporarily or permanently, the Application or any service to which it connects, with or without notice and without liability to you.<br/><br/></div>
+        <div className='grayText'><strong>Term and Termination</strong></div>
+        <div className='innerText'>This Agreement shall remain in effect until terminated by you or "Buddy Ghee LLC".  "Buddy Ghee LLC", may, in its sole discretion, at any time and for any or no reason, suspend or terminate this Agreement with or without prior notice. This Agreement will terminate immediately, without prior notice from "Buddy Ghee LLC", in the event that you fail to comply with any provision of this Agreement. You may also terminate this Agreement by deleting the Application and all copies thereof from your mobile device or from your desktop. Upon termination of this Agreement, you shall cease all use of the Application and delete all copies of the Application from your mobile device or from your desktop.</div><br/>
+        <div className='grayText'><strong>Severability</strong></div>
+        <div className='innerText'>If any provision of this Agreement is held to be unenforceable or invalid, such provision will be changed and interpreted to accomplish the objectives of such provision to the greatest extent possible under applicable law and the remaining provisions will continue in full force and effect.</div><br/>
+        <div className='grayText'><strong>Amendments to this Agreement</strong></div>
+        <div className='innerText'>"Buddy Ghee LLC", reserves the right, at its sole discretion, to modify or replace this Agreement at any time. If a revision is material we will provide at least 30 days notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.</div>
+        <span id='ourCon'></span><br/>
+        <div className='blueText'><strong>Contacting Us</strong></div>
+        <div className='innerText'>If there are any questions regarding this Agreement, you may contact us using the information below.<br/><br/></div>
+        <div className='innerText'>noah@noahschutte.com</div>
+        <div className='innerText'><br/>Last Edited on 2017-3-25</div><br/>
+      </div>
+    </div>
+  )
+}
+
+export default EULA;
